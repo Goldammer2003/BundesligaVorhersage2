02c3ca768c45178893e9914baae57c3c0576b010
Basil902 <Basil902@users.noreply.github.com>
2025-06-13 01:31:22 +0200

    Remove debug route

--- bundesliga-predictor/run.py
+++ bundesliga-predictor/run.py
@@ -2,9 +2,5 @@ from app import create_app
 
 app = create_app()
 
-@app.route('/debug')
-def debug():
-    return str(app.static_folder)
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
